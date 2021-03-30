558ed3ff39586ae12823b7d1f96f3a75b9478b99
Mitch <ddiarikov@gmail.com>
2021-03-30 21:01:48 +0300

    FollowPath.cs

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -1,80 +1,44 @@
 ﻿using UnityEngine;
 
-public class FollowPath : MonoBehaviour {
-
-    // Tank targer
-    Transform goal;
-    // Tank speed
-    float speed = 5.0f;
-    // Final distance from target
-    float accuracy = 1.0f;
-    // Tank rotation speed
-    float rotSpeed = 2.0f;
-    // Access to the WPManager script
-    public GameObject wpManager;
-    // Array of waypoints
-    GameObject[] wps;
-    // Current waypoint
+public class FollowPath : MonoBehaviour 
+{
+    [SerializeField] Transform goal;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float accuracy = 1.0f;
+    [SerializeField] float rotSpeed = 2.0f;
+    [SerializeField] GameObject wpManager;
+    GameObject[] waypoints;
     GameObject currentNode;
-    // Starting waypoint index
     int currentWP = 0;
-    // Access to the Graph script
-    Graph g;
+    Graph graph;
 
-    // Use this for initialization
-    void Start() {
-
-        // Get hold of wpManager and Graph scripts
-        wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
-        // Set the current Node
-        currentNode = wps[7];
-    }
-
-    public void GoToHeli() {
-
-        // Use the AStar method passing it currentNode and distination
-        g.AStar(currentNode, wps[4]);
-        // Reset index
-        currentWP = 0;
-    }
-
-    public void GoToRuin() {
-
-        // Use the AStar method passing it currentNode and distination
-        g.AStar(currentNode, wps[0]);
-        // Reset index
-        currentWP = 0;
-    }
-
-    public void GoBehindHeli() {
-
-        // Use the AStar method passing it currentNode and distination
-        g.AStar(currentNode, wps[11]);
-        // Reset index
-        currentWP = 0;
+    void Start() 
+    {
+        waypoints = wpManager.GetComponent<WPManager>().waypoints;
+        graph = wpManager.GetComponent<WPManager>().graph;
+        currentNode = waypoints[7];
     }
 
     // Update is called once per frame
     void LateUpdate() {
 
         // If we've nowhere to go then just return
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+        if (graph.getPathLength() == 0 || currentWP == graph.getPathLength())
             return;
 
         //the node we are closest to at this moment
-        currentNode = g.getPathPoint(currentWP);
+        currentNode = graph.getPathPoint(currentWP);
 
         //if we are close enough to the current waypoint move to next
         if (Vector3.Distance(
-            g.getPathPoint(currentWP).transform.position,
+            graph.getPathPoint(currentWP).transform.position,
             transform.position) < accuracy) {
             currentWP++;
         }
 
         //if we are not at the end of the path
-        if (currentWP < g.getPathLength()) {
-            goal = g.getPathPoint(currentWP).transform;
+        if (currentWP < graph.getPathLength()) {
+            goal = graph.getPathPoint(currentWP).transform;
             Vector3 lookAtGoal = new Vector3(goal.position.x,
                                             this.transform.position.y,
                                             goal.position.z);
