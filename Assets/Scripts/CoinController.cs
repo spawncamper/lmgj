54e39f30030e2c8a92b9fe9c47912df50f7f6e8d
Mitch <ddiarikov@gmail.com>
2021-04-02 17:36:07 +0300

    папку скриптов организовал.  монетки работают,

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,28 +4,64 @@ public class CoinController : MonoBehaviour
 {
     GameObject metal;
     [SerializeField] int initialCoins = 3;
+    [SerializeField] GameObject coin;
+    int currentCoins;
 
-    public GameObject coin;
+//    public delegate void CoinSpawned();
+//    public static event CoinSpawned CoinSpawnedEvent;
 
-    public delegate void CoinSpawned();
-    public static event CoinSpawned CoinSpawnedEvent;
+    public delegate void ScoreChanged();
+    public static event ScoreChanged ScoreChangedEvent;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("space"))
         {
-            Vector3 CoinPosition = new Vector3(transform.position.x, 0.65f, transform.position.z);
-            Instantiate(coin, CoinPosition, Quaternion.identity);
+            if (currentCoins >= 1)
+            {
+                Vector3 CoinPosition = new Vector3(transform.position.x, 0.65f, transform.position.z);
+                Instantiate(coin, CoinPosition, Quaternion.identity);
 
-            if (CoinSpawnedEvent != null)
-                CoinSpawnedEvent();
+                if (ScoreChangedEvent != null)
+                    ScoreChangedEvent();
+
+                currentCoins--;
+            }
+            else
+            {
+                Debug.LogWarning("[CoinController] currentCoins = 0");
+            }
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        currentCoins = initialCoins;
+
+        if (ScoreChangedEvent != null)
+            ScoreChangedEvent();
+    }
+
+    public void AddCoins(int number)
+    {
+        currentCoins = currentCoins + number;
+
+        if (ScoreChangedEvent != null)
+            ScoreChangedEvent();
+    }
+
+    public int ReturnCurrentCoins()
+    {
+        return currentCoins;
+    }
+
+    void ResetCurrentCoins()
+    {
+        currentCoins = 0;
+
+        if (ScoreChangedEvent != null)
+            ScoreChangedEvent();
     }
 }
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] string Level01;
+    [SerializeField] string mainMenu;
+    [SerializeField] string bootScene;
+    [SerializeField] float delay = 0.5f;
+    [SerializeField] TMP_Text scoreText;
+
+    SceneLoader sceneLoader;
+    CoinController coinController;
+
+    string activeScene;
+    int currentCoins;
+    Scene[] loadedScenes;
+
+    void OnEnable()
+    {
+        GameManager.PlayerSpawnedEvent += FindObjectOfTypeCoinController;
+        CoinController.ScoreChangedEvent += UpdateScore;
+    }
+
+    void OnDisable()
+    {
+        GameManager.PlayerSpawnedEvent -= FindObjectOfTypeCoinController;
+        CoinController.ScoreChangedEvent -= UpdateScore;
+    }
+
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+
+        //       activeScene = SceneManager.GetActiveScene().name;
+
+        //       print("[UIManager] activeScene " + activeScene);
+        //        buildIndex = activeScene.buildIndex;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("press Esc");
+
+            GoToMainMenu();
+        }
+    }
+
+    public void StartGameButton()
+    {
+        sceneLoader.LoadLevelAsync(Level01);
+        sceneLoader.UnloadLevelAsync(mainMenu);
+    }
+
+    public void QuitGameButton()
+    {
+        sceneLoader.QuitGame();
+    }
+
+    void GoToMainMenu()
+    {
+        loadedScenes = sceneLoader.GetOpenScenes();
+
+        foreach (Scene scene in loadedScenes)
+        {
+            activeScene = scene.name;
+
+            print(activeScene);
+
+            if (activeScene == bootScene)
+            {
+                continue;
+            }
+
+            if (activeScene == mainMenu)
+            {
+                continue;
+            }
+
+            Debug.Log("[UIManager] Update loop, foreach Scene scene in loadedScenes");
+
+            StartCoroutine(GoToMainMenuCoroutine());
+
+            break;
+        }
+    }
+
+    IEnumerator GoToMainMenuCoroutine()
+    {
+        sceneLoader.LoadLevelAsync(mainMenu);
+        yield return new WaitForSeconds(delay);
+        sceneLoader.UnloadLevelAsync(loadedScenes[1].name);
+        yield return new WaitForSeconds(delay);
+    }
+
+    void UpdateScore()
+    {
+        currentCoins = coinController.ReturnCurrentCoins();
+
+        scoreText.text = currentCoins.ToString();
+    }
+
+    void WriteToTextmeshpro(string inputString)
+    {
+        scoreText.text = inputString.ToString();
+    }
+
+    void FindObjectOfTypeCoinController()
+    {
+        coinController = FindObjectOfType<CoinController>();
+
+        if (coinController == null)
+        {
+            Debug.LogError("[UIManager] coinController == null");
+        }
+    }
+}
