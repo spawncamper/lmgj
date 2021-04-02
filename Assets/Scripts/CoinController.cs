ba288602942b69a47c36bc5c84b1a232eb9a7c26
Mitch <ddiarikov@gmail.com>
2021-04-02 17:09:13 +0300

    Player_V2, организовал папку Scripts,

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
@@ -7,15 +5,27 @@ public class CoinController : MonoBehaviour
     GameObject metal;
     [SerializeField] int initialCoins = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    public GameObject coin;
+
+    public delegate void CoinSpawned();
+    public static event CoinSpawned CoinSpawnedEvent;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 CoinPosition = new Vector3(transform.position.x, 0.65f, transform.position.z);
+            Instantiate(coin, CoinPosition, Quaternion.identity);
+
+            if (CoinSpawnedEvent != null)
+                CoinSpawnedEvent();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
     }
 }
